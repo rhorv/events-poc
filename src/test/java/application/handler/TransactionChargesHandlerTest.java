c796698dfed67fb3d9e7d5b161761294cecd0ae7
Rudolf Horvath <rhorvath@starfleet.hu>
2021-02-25 16:25:07 +0000

    Renamed a test

--- src/test/java/application/handler/TransactionChargesHandlerTest.java
+++ src/test/java/application/handler/TransactionChargesHandlerTest.java
@@ -18,7 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-class DoSomethingHandlerTest {
+class TransactionChargesHandlerTest {
 
   private class StubPublisher implements IPublish {
 
